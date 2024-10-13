bd6b1fa6156504afe95c660fce4f6b2d3071fc49
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2024-10-13 16:56:53 +0700

    Done

---
+++ CalculatorViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorViewModel.swift
+//  Eltex_Calculator
+//
+//  Created by Леонид Шайхутдинов on 13.10.2024.
+//
+
+import Foundation
---
+++ Eltex_Calculator/RoundButton.swift
@@ -0,0 +1,8 @@
+//
+//  RoundButton.swift
+//  Eltex_Calculator
+//
+//  Created by Леонид Шайхутдинов on 09.10.2024.
+//
+
+import Foundation
