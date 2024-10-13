5fa8ac803f4547eacf22b48729f390bdb81b5ea6
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2024-10-13 16:58:40 +0700

    Done

--- CalculatorViewModel.swift
+++ CalculatorViewModel.swift
@@ -6,3 +6,84 @@
 //
 
 import Foundation
+
+class CalculatorViewModel {
+    var input: String = ""
+    var operators: [Character] = ["/", "*", "+", "-", "%"]
+    
+    var onInputUpdate: ((String) -> Void)?
+    var onResultUpdate: ((String) -> Void)?
+    
+    func isLastCharOperator() -> Bool {
+        if let lastChar = input.last {
+            return operators.contains(lastChar)
+        }
+        return false
+    }
+    
+    func isFirstCharOperator() -> Bool {
+        if let firstChar = input.first {
+            if firstChar == "*" || firstChar == "/" {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func validInput() -> Bool {
+        if isLastCharOperator() {
+            return false
+        }
+        if isFirstCharOperator() {
+            return false
+        }
+        return true
+    }
+    
+    func pushForwardInput(value: String) {
+        input = value + input
+        onInputUpdate?(input)
+    }
+    
+    
+    func pushBackToInput(value: String) {
+        input += value
+        onInputUpdate?(input)  // Обновляем интерфейс через callback
+    }
+    
+    func calculateResult() {
+        if validInput() {
+            let formattedInput = input.replacingOccurrences(of: "/", with: "*1.0/")
+            let expression = NSExpression(format: formattedInput)
+            if let result = expression.expressionValue(with: nil, context: nil) as? Double {
+                let formattedResult = formatResult(result: result)
+                onResultUpdate?(formattedResult)  // Обновляем результат через callback
+            }
+        } 
+        else {
+            print("Invalid input")
+        }
+    }
+    
+    func formatResult(result: Double) -> String {
+        if result.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "=%.0f", result)
+        } else {
+            return String(format: "=%.6f", result)
+        }
+    }
+    
+    func clearAll() {
+        input = ""
+        onInputUpdate?("")
+        onResultUpdate?("")
+    }
+    
+    func deleteLast() {
+        if !input.isEmpty {
+            input.removeLast()
+            onInputUpdate?(input)
+        }
+    }
+}
+
--- Eltex_Calculator/ViewController.swift
+++ Eltex_Calculator/ViewController.swift
@@ -6,14 +6,107 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBOutlet weak var inputLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var viewModel = CalculatorViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Подписываемся на обновления от ViewModel
+        viewModel.onInputUpdate = { [weak self] input in
+            self?.inputLabel.text = input
+        }
+        viewModel.onResultUpdate = { [weak self] result in
+            self?.resultLabel.text = result
+        }
     }
 
+    @IBAction func percentTap(_ sender: Any) {
+        if !viewModel.isLastCharOperator() {
+            viewModel.pushForwardInput(value: "(")
+            viewModel.pushBackToInput(value: ")")
+            viewModel.pushBackToInput(value: "*0.01")
+            
+        }
+        viewModel.calculateResult()
+    }
+    
+    @IBAction func divideTap(_ sender: Any) {
+        if !viewModel.isLastCharOperator() && !viewModel.input.isEmpty {
+            viewModel.pushBackToInput(value: "/")
+        }
+        
+    }
+    @IBAction func sevenTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "7")
+    }
+    
+    @IBAction func eightTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "8")
+    }
+    
+    @IBAction func nineTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "9")
+    }
+    @IBAction func multiplyTap(_ sender: Any) {
+        if !viewModel.isLastCharOperator() && viewModel.input.count != 0{
+            viewModel.pushBackToInput(value: "*")
+        }
+        
+    }
+    @IBAction func fourTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "4")
+    }
+    @IBAction func fiveTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "5")
+    }
+    @IBAction func sixTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "6")
+    }
+    @IBAction func minusTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "-")
+    }
+    @IBAction func oneTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "1")
+    }
+    @IBAction func twoTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "2")
+    }
+    @IBAction func threeTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "3")
+    }
+    @IBAction func plusTap(_ sender: Any) {
+        if !viewModel.isLastCharOperator() {
+            viewModel.pushBackToInput(value: "+")
+        }
+    }
+    @IBAction func zeroTap(_ sender: Any) {
+        viewModel.pushBackToInput(value: "0")
+    }
+    @IBAction func comaTap(_ sender: Any) {
+        if let lastChar = viewModel.input.last, lastChar != "." {
+            viewModel.pushBackToInput(value: ".")
+        }
+        
+    }
+    @IBAction func equalTap(_ sender: Any) {
+        viewModel.calculateResult()
+    }
+    
+    @IBAction func clearTap(_ sender: Any) {
+        viewModel.clearAll()
+    }
+    
+    @IBAction func deleteTap(_ sender: Any) {
+        viewModel.deleteLast()
+    }
 
 }
 
